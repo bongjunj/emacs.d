33b7019268ae9048dbcf65d0d3c22ee3c5abe6d2
Bongjun <bongjun.jang@kaist.ac.kr>
2026-08-24 22:09:03 +0900

    font 180

--- init.el
+++ init.el
@@ -185,13 +185,13 @@
 
 (set-face-attribute 'default nil
                     :family "Iosevka"
-                    :height 160
+                    :height 180
                     :weight 'regular
                     :slant 'normal)
 
 (set-face-attribute 'variable-pitch nil
                     :family "Source Serif 4"
-                    :height 160
+                    :height 180
                     :weight 'regular
                     :slant 'normal)
 
